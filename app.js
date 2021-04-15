fca736ac34b2d45af01d1ecb3f7a1b209334464e
MiguelED <migueled2695@gmail.com>
2021-04-15 05:11:23 -0500

    importing npm modules

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
-/*
-const add = require( './utils.js' );
+const validator = require( 'validator' );
 
-const sum = add( 4 , 2 );
-
-console.log( sum );
-*/
-
-/*
-Challenge: Define and use a function in a new file
-    1.- Create a new file called notes.js
-    2.- Create getNotes function that returns 'Your notes ...'
-    3.- Export getNotes function
-    4.- From app.js load in and call the function printing message to console
-*/
-
-const getNotes = require( './notes.js' );
-
-const notes = getNotes();
-
-console.log( notes );
+console.log( validator.isEmail( 'miguel@gmail.com' ) );
+console.log( validator.isURL( 'https://google.com' ) );
