4559267e8732ec277641436c3f890ce9fd6641e6
MiguelED <migueled2695@gmail.com>
2021-04-15 20:50:55 -0500

    Printing color

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-const validator = require( 'validator' );
+/*
+Challenge: Use the chalk library in you project
+    1.- Install version 2.4.1 of chalk
+    2.- Lood chalk info app.js
+    3.- Use it to print the string "Success" to the console in green
+    4.- Test your work
 
-console.log( validator.isEmail( 'miguel@gmail.com' ) );
-console.log( validator.isURL( 'https://google.com' ) );
+    Bonus: Use docs to mess around with other styles. Make text bold and inversed
+*/
+
+const chalk = require('chalk');
+ 
+const msg = 'Success';
+const styleMsg = chalk.black.bold.bgGreen( msg );
+
+console.log( styleMsg );
