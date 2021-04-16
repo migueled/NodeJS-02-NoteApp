2b035db8c8cb46913751b8a176c2bef5740363c1
MiguelED <migueled2695@gmail.com>
2021-04-16 02:31:59 -0500

    Argument parsing with yargs

--- app.js
+++ app.js
@@ -4,11 +4,31 @@ const yargs = require( 'yargs' );
 //-- customize yargs version
 yargs.version( '1.1.0' );
 
+/*
+Challenge: Add an option to yargs
+    1.- Setup a body option for the add command
+    2.- Configure a description, make it required, and for it to be a string
+    3.- Log the body value in the handler function
+    4.- Test your work
+*/
+
 //--Create add command
 yargs.command({
     command :   'add',
     describe:   'Add a new note',
-    handler:    () => console.log( 'Adding a new note!' )
+    builder :   {
+        title : { 
+            describe : 'Note title',
+            demandOption : true,
+            type : 'string'
+        },
+        body : {
+            describe : 'Body of note',
+            demandOption : true,
+            type : 'string'
+        }
+    },
+    handler :    ( argv ) => console.log( 'Title: ' , argv.title , 'Body: ' , argv.body )
 });
 
 //-- Create remove command
@@ -17,13 +37,6 @@ yargs.command({
     describe: 'Remove a note',
     handler : () => console.log( 'Removing thr note' )
 });
-
-/*
-Challenge: Add two new commands
-    1.- Setup command to support 'list' command ( print placeholder message for new)
-    2.- Setup command to support 'read' command ( print placeholder message for new)
-    3.- Test your work by running both commands and ensure correct output
-*/
 
 //Create list command
 yargs.command({
@@ -40,4 +53,4 @@ yargs.command({
 });
 
 //--Add , Remove , Read , List
-console.log( yargs.argv );
+yargs.parse();
